2076ef0ff7b9abf32b5ed5e4d3560d276280a90f
Abdulsamad Rabo <raboabdulsamad04@gmail.com>
2023-09-29 15:05:10 +0100

    First

---
+++ musicplayer.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import filedialog
+import pygame
+from pygame import mixer
+
+# Initialize pygame mixer
+mixer.init()
+
+# Create the main application window
+app = tk.Tk()
+app.title("Music Player")
+
+# Function to open a file and play it
+def play_music():
+    filepath = filedialog.askopenfilename(filetypes=[("Audio Files", "*.mp3 *.wav")])
+    if filepath:
+        mixer.music.load(filepath)
+        mixer.music.play()
+
+# Function to stop the currently playing music
+def stop_music():
+    mixer.music.stop()
+
+# Create and place widgets
+play_button = tk.Button(app, text="Play", command=play_music)
+play_button.pack()
+
+stop_button = tk.Button(app, text="Stop", command=stop_music)
+stop_button.pack()
+
+# Start the main loop
+app.mainloop()
